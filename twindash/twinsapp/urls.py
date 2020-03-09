afb8e4f11e20b562935edbd98ac5bf0833090894
Pablo Arnalte-Mur <pablo.arnalte@gmail.com>
2020-03-09 01:26:08 +0100

    Add basic day navigation and re-organise baby-day views

--- twindash/twinsapp/urls.py
+++ twindash/twinsapp/urls.py
@@ -5,8 +5,8 @@ from . import views
 app_name = 'twinsapp'
 urlpatterns = [
     path('', views.index, name='index'),
-    # ex. /twinsapp/bebe/MAIA/
-    path('bebe/<nombre_bebe>/', views.main_bebe, name='main_bebe'),
-    # ex. /twinsapp/2020/02/17/
-    path('<int:year>/<int:month>/<int:day>/', views.detail_day, name='detail_day')
+    # ex. /twinsapp/MAIA/
+    path('<nombre_bebe>/', views.main_bebe, name='main_bebe'),
+    # ex. /twinsapp/MAIA/2020/03/01/
+    path('<nombre_bebe>/<int:year>/<int:month>/<int:day>/', views.bebe_dia, name='bebe_dia'),
 ]
--- twindash/twinsapp/views.py
+++ twindash/twinsapp/views.py
@@ -22,40 +22,48 @@ def index(request):
 
 
 @login_required(login_url='/admin/login/')
-def main_bebe(request, nombre_bebe):
+def bebe_dia(request, nombre_bebe, year, month, day):
     bebe = get_object_or_404(Bebe, nombre=nombre_bebe)
     lista_bebes = Bebe.objects.all()
+    fecha = datetime.date(year, month, day)
     dia_hoy = datetime.date.today()
 
-    tomas_bebe_hoy = Toma.objects.filter(fecha__date=dia_hoy, bebe = bebe)
+    tomas_bebe_hoy = Toma.objects.filter(fecha__date=fecha, bebe = bebe).order_by("-fecha")
     n_tomas_hoy = tomas_bebe_hoy.count()
     cantidad_bibe_hoy = (
         sum([x['cantidad_artificial'] for x in tomas_bebe_hoy.values()]) +
         sum([x['cantidad_materna'] for x in tomas_bebe_hoy.values()]) )
     n_tomas_teta_hoy = tomas_bebe_hoy.filter(toma_teta=True).count()
 
-    cambios_bebe_hoy = Cambio.objects.filter(fecha__date=dia_hoy, bebe = bebe)
+    cambios_bebe_hoy = Cambio.objects.filter(fecha__date=fecha, bebe = bebe).order_by("-fecha")
     n_cambios_hoy = cambios_bebe_hoy.count()
+    n_cambios_hoy_caca = cambios_bebe_hoy.filter(caca=True).count()
     ultimo_cambio_caca = Cambio.objects.filter(caca=True, bebe=bebe).order_by("fecha").last()
 
+    es_hoy = (fecha == dia_hoy)
+    fecha_anterior = fecha - datetime.timedelta(days=1)
+    fecha_posterior = fecha + datetime.timedelta(days=1)
     context = {
         'bebe': bebe,
         'bebe_list': lista_bebes,
-        'hoy': dia_hoy,
+        'fecha': fecha,
+        'es_hoy': es_hoy,
+        'fecha_anterior': fecha_anterior,
+        'fecha_posterior': fecha_posterior,
         'latest_tomas_list': tomas_bebe_hoy,
         'n_tomas': n_tomas_hoy,
         'cantidad_bibe': cantidad_bibe_hoy,
         'n_tomas_teta': n_tomas_teta_hoy,
         'latest_cambios_list': cambios_bebe_hoy,
         'n_cambios': n_cambios_hoy,
+        'n_cambios_caca': n_cambios_hoy_caca,
         'ultima_caca': ultimo_cambio_caca,
     }
-    return render(request, 'twinsapp/bebe.html', context)
+    return render(request, 'twinsapp/bebe_dia.html', context)
+
 
 
 @login_required(login_url='/admin/login/')
-def detail_day(request, year, month, day):
-    date = datetime.date(year, month, day)
-    tomas_dia = Toma.objects.filter(fecha__date=date)
-    output = "<br> ".join([str(t) for t in tomas_dia])
-    return HttpResponse(output)
+def main_bebe(request, nombre_bebe):
+    dia_hoy = datetime.date.today()
+    return bebe_dia(request, nombre_bebe, dia_hoy.year, dia_hoy.month, dia_hoy.day)
